f92520b1e535d3bb059af4b3ef15b08d24df8bda
André Vítor Silva Melo <andre.vsm@hotmail.com>
2018-04-07 11:32:59 -0300

    Transferências

---
+++ MazeBankWPF/View/Transferencias.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View {
+    /// <summary>
+    /// Interaction logic for Transferencias.xaml
+    /// </summary>
+    public partial class Transferencias : Window {
+        public Transferencias() {
+            InitializeComponent();
+        }
+       
+    }
+}
