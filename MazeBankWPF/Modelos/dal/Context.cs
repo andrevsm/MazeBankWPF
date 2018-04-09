6037a6651dcd884243ac6c61e9e35b71ef6317c6
André Vítor Silva Melo <andre.vsm@hotmail.com>
2018-04-09 20:33:52 -0300

    Context class criada na pasta DAL.

---
+++ MazeBankWPF/Modelos/dal/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DAL { 
+    public class Context : DbContext {
+        public Context() : base("strConn") {
+                   
+        }
+
+        public DbSet<Cliente> tblClientes { get; set; }
+        public DbSet<Conta> tblContas { get; set; }
+        public DbSet<Endereco> tblEndereco { get; set; }
+    }
+}
