59f96c5cf193589df1561a7db41b0e47eb3aa77e
André Vítor S. Melo <andre.vsm@hotmail.com>
2018-04-12 10:25:50 -0300

    Criado o Construtor da classe.

--- MazeBankWPF/Modelos/Cliente.cs
+++ MazeBankWPF/Modelos/Cliente.cs
@@ -8,12 +8,19 @@ namespace Models
 {
     public class Cliente
     {
+        public Cliente() {
+            _Endereco = new Endereco();
+            _Conta = new Conta();
+        }
+
         public int ClienteId { get; set; }
         public string Nome { get; set; }
         public string Nascimento { get; set; }
         public string Cpf { get; set; }
         public int ContaId { get; set; }
+        public Conta _Conta { get; set; }
         public int EnderecoId { get; set; }
+        public Endereco _Endereco { get; set; }
     }
 
 }
