5b720415ce62f59f09ebbfff441661f8f53398f7
Miguel Lorenzi <miguelml159357@gmail.com>
2018-04-18 20:19:04 -0300

    Mexendo no Excluir.

--- MazeBankWPF/View/MinhaConta.xaml.cs
+++ MazeBankWPF/View/MinhaConta.xaml.cs
@@ -14,16 +14,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace View {
+namespace View
+{
     /// <summary>
     /// Interaction logic for MinhaConta.xaml
     /// </summary>
-    public partial class MinhaConta : Window {
+    public partial class MinhaConta : Window
+    {
 
         private static Conta minhaConta;
         private static Cliente meuCliente;
 
-        public MinhaConta(Conta contaCliente, Cliente cliente) {
+        public MinhaConta(Conta contaCliente, Cliente cliente)
+        {
             minhaConta = contaCliente;
             meuCliente = cliente;
             InitializeComponent();
@@ -31,43 +34,47 @@ namespace View {
 
 
 
-        private void btn_Saldo(object sender, RoutedEventArgs e) {
+        private void btn_Saldo(object sender, RoutedEventArgs e)
+        {
             MessageBoxResult saldo = MessageBox.Show("Saldo: R$" + minhaConta.Saldo);
         }
 
-        private void btn_Deposito(object sender, RoutedEventArgs e) {
+        private void btn_Deposito(object sender, RoutedEventArgs e)
+        {
             Depositos depositos = new Depositos(minhaConta);
             depositos.Show();
         }
 
-        private void btn_Pagamentos(object sender, RoutedEventArgs e) {
+        private void btn_Pagamentos(object sender, RoutedEventArgs e)
+        {
 
         }
 
-        private void btn_Transferencias(object sender, RoutedEventArgs e) {
+        private void btn_Transferencias(object sender, RoutedEventArgs e)
+        {
             Transferencias transferencias = new Transferencias(minhaConta);
             transferencias.Show();
         }
 
-        private void btn_EditConta(object sender, RoutedEventArgs e) {
+        private void btn_EditConta(object sender, RoutedEventArgs e)
+        {
 
         }
 
-        private void btn_Excluir(object sender, RoutedEventArgs e) {
-            MessageBoxResult result = MessageBox.Show("Deseja realmente exluir a conta?","Excluir conta", MessageBoxButton.YesNo);
+        private void btn_Excluir(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Deseja realmente exluir a conta?", "Excluir conta", MessageBoxButton.YesNo);
             if (result == MessageBoxResult.Yes)
             {
-                MessageBoxResult res = MessageBox.Show("APERTOU YES");
-            } else
-            {
-
-            }
-            //ContaController cc = new ContaController();
-            //cc.ExcluirConta(minhaConta.ContaID);
-            //EnderecoController ec = new EnderecoController();
-            //ec.ExcluirEndereco(meuCliente.EnderecoID);
-            //ClienteController cli = new ClienteController();
-            //cli.ExcluirCliente(meuCliente.ClienteID);
+                ContaController cc = new ContaController();
+                cc.ExcluirConta(minhaConta.ContaID);
+                EnderecoController ec = new EnderecoController();
+                ec.ExcluirEndereco(meuCliente.EnderecoID);
+                ClienteController cli = new ClienteController();
+                cli.ExcluirCliente(meuCliente.ClienteID);
+                MessageBoxResult res = MessageBox.Show("Conta excluída com sucesso!");
+                Close();
+            }            
         }
     }
 }
