ae65e3857fb85bb81de4c5f39e86a5cacbb8117e
André Vítor S. Melo <andre.vsm@hotmail.com>
2018-04-19 10:02:12 -0300

    Criado model da conta.

---
+++ MazeBankWPF/Modelos/ContasPagas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ContasPagas
+    {
+        public int ContaPagasID { get; set; }
+        public int Boleto { get; set; }
+        public double Valor { get; set; }
+        public int ContaID { get; set; }
+        public Conta _Conta { get; set; }
+    }
+
+}
