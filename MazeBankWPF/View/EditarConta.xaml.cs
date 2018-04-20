a6fc8c828793af2d454b0616cc1e4c4641267c5c
Miguel Lorenzi <miguelml159357@gmail.com>
2018-04-20 16:52:23 -0300

    Criação EditarContas

---
+++ MazeBankWPF/View/EditarConta.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Controller;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View {
+    /// <summary>
+    /// Interaction logic for Depositos.xaml
+    /// </summary>
+    public partial class Depositos : Window {
+        private static Conta conta;
+
+        public Depositos(Conta minhaConta) {
+            conta = minhaConta;
+            InitializeComponent();
+        }
+
+        private void btnDepositar_Click(object sender, RoutedEventArgs e)
+        {
+            conta.Saldo = Double.Parse(txt_Valor.Text);
+            ContaController cc = new ContaController();
+            cc.EditarConta(conta);
+            MessageBoxResult deposito = MessageBox.Show("Depósito realizado com sucesso!");
+            Close();
+        }
+    }
+}
